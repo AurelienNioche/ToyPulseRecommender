1d96e7305875d7df453871a82fdd758f7abb89f1
AurelienNioche <nioche.aurelien@gmail.com>
2022-03-08 19:48:10 +0000

    update

---
+++ listen_to_arduino.py
@@ -0,0 +1,50 @@
+import serial
+import json
+
+
+import matplotlib.pyplot as plt
+
+
+def main():
+
+	arduino = serial.Serial('/dev/cu.usbmodem112201', 115200, timeout=.1)
+	fig, ax = plt.subplots()
+	lines, = ax.plot([], [])
+
+	timestamp = []
+	bpm = []
+	spO2 = []
+	while True:
+		data = arduino.readline().strip()
+		if data:
+			data = data.decode()
+			try:
+				dic_data = json.loads(data)
+				print("timestamp", dic_data["timestamp"],
+					  "bpm", dic_data["bpm"],
+					  "spO2", dic_data["spO2"])
+				timestamp.append(dic_data["timestamp"])
+				bpm.append(dic_data["bpm"])
+				spO2.append(dic_data["spO2"])
+				#ax.plot(timestamp, bpm)
+				#fig.canvas.draw_idle()
+				lines.set_xdata(timestamp)
+				lines.set_ydata(bpm)
+
+				# Need both of these in order to rescale
+				ax.relim()
+				ax.autoscale_view()
+
+				fig.canvas.draw_idle()
+				fig.canvas.flush_events()
+
+				plt.pause(0.05)
+
+			except (UnicodeDecodeError, json.decoder.JSONDecodeError):
+				print("Not data", data)
+
+
+if __name__ == "__main__":
+	main()
+
+
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-import serial
-import json
-arduino = serial.Serial('/dev/cu.usbmodem112201', 115200, timeout=.1)
-while True:
-	data = arduino.readline().strip()
-	if data:
-		data = data.decode()
-		try:
-			dic_data = json.loads(data)
-			print("timestamp", dic_data["timestamp"],
-				  "bpm", dic_data["bpm"],
-				  "spO2", dic_data["spO2"])
-
-		except (UnicodeDecodeError, Exception):
-			print("Not data", data)
+def main():
+	pass
 
 
+if __name__ == "__main__":
+	main()
